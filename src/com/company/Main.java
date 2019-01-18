fc5ec61122d18a6e0e858e8ec88c7c0e79423ee3
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-18 08:55:06 -0500

    Use a for loop to check for the smallest number divisible by numbers 1-20.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int number = 1;
+        for(int i = 1; i < 20; i++){
+            if(number % i != 0){
+                i = 0;
+                number++;
+            }
+        }
+        System.out.println(number);
     }
 }
